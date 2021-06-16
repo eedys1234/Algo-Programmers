cf7decd43564a323cb71daa43139f6794b9bcd73
이정환 <eedys1234@naver.com>
2021-06-17 08:10:54 +0900

    풍선 터트리기 풀이

---
+++ src/main/java/PS_방의개수.java
@@ -0,0 +1,11 @@
+public class PS_방의개수 {
+
+    public static void main(String... args) throws Exception {
+
+    }
+
+    public static int solution(int[] arrows) {
+        int answer = 0;
+        return answer;
+    }
+}
--- src/main/java/PS_풍선터트리기.java
+++ src/main/java/PS_풍선터트리기.java
@@ -1,12 +1,41 @@
+
+
 public class PS_풍선터트리기 {
 
     public static void main(String... args) throws Exception {
+
+        int[] a = {-16, 27, 65, -2, 58, -92, -71, -68, -61, -33};
+        int answer = solution(a);
+        System.out.println(answer);
 
     }
 
     public static int solution(int[] a) {
         int answer = 0;
+        int[] left_mins = new int[a.length];
+        int[] right_mins = new int[a.length];
 
-        return answer;
+        int left_min = Integer.MAX_VALUE;
+        int right_min = Integer.MAX_VALUE;
+
+        for(int i=0;i<a.length;i++)
+        {
+            int j = a.length - 1 - i;
+
+            right_min = Math.min(right_min, a[j]);
+            right_mins[j] = right_min;
+
+            left_min = Math.min(left_min, a[i]);
+            left_mins[i] = left_min;
+        }
+
+        for(int i=0;i<a.length;i++)
+        {
+            if(i>0 && i<a.length-1 && (left_mins[i-1] > a[i] || a[i] < right_mins[i+1])) {
+                answer++;
+            }
+        }
+
+        return answer+2;
     }
 }
